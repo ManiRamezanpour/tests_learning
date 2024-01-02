45455c383e9b53ff7f6d2d8487246c80d7acfb31
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2024-01-02 23:53:56 +0330

    add react-testing-library and jsdom

--- react-testing/src/components/header/header.spec.tsx
+++
@@ -1,7 +0,0 @@
-// import { createRenderer } from "react-dom/test-utils";
-// import { test } from "vitest";
-// import Header from "./Header";
-// const headerTitle: string = "HELLO WORLD";
-// test("Header Test", () => {
-//   createRenderer(<Header title={headerTitle} />);
-// });
--- react-testing/tests/App.test.tsx
+++ react-testing/tests/App.test.tsx
@@ -1,9 +1,13 @@
-import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
+import React from "react";
+import { describe, expect, it } from "vitest";
+import App from "../src/App";
 
 // Tests
 describe("App test", async () => {
   it("Should render the page correctly", async () => {
-    expect(true).toBeTruthy();
+    render(<App />);
+    const h1 = screen.queryByText("HELLO WORLD");
+    expect(h1).not.toBeNull();
   });
 });
